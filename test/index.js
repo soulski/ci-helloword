82e38d5d0317250e1e5da30bf7f766a695771f4b
SouLsKi <soulski@gmail.com>
2014-07-19 11:42:41 +0700

    add test file

---
+++ test/index.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+
+describe('test', function () {
+	describe('hello world', function() {
+		it('should success', function (done) {
+			assert.equal(true, true);
+			done();
+		})
+	})
+});
