32f6e8bc3bc7f00aa59d46756868c2dc0cfc3f20
SouLsKi <soulski@gmail.com>
2014-07-19 15:10:43 +0700

    add human test

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,15 @@
-var should = require('should');
+var should = require('should')
+		app = require('../app/index');
 
 describe('test', function () {
 	describe('hello world', function() {
-		it('should success', function (done) {
-			true.should.be.true;
+		it('should say hello', function (done) {
+			app.sayHello().should.equal('hello');
+			done();
+		})
+
+		it('should be robot', function (done) {
+			app.isHuman().should.be.false;
 			done();
 		})
 	})
