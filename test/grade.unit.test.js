60a2dc46169e4a360a47e5c7cd7492d289111a52
vkiet <veerakiet4@gmail.com>
2014-07-19 16:03:00 +0700

    add test grace D+

--- test/grade.unit.test.js
+++ test/grade.unit.test.js
@@ -14,5 +14,17 @@ describe('GradeUnitTest', function () {
 		it('should return A- when score is greater than 80', function () {
 			grade(82).should.equal('A-');
 		});
+
+		it('should return B when score is greater than 70', function () {
+			grade(73).should.equal('B');
+		});
+
+		it('should return C when score is greater than 64', function () {
+			grade(68).should.equal('C');
+		});
+
+		it('should return D+ when score is greater than 59', function () {
+			grade(61).should.equal('D+');
+		});
 	});
 });
