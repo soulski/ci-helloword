4f24372c477594ba08a0bbd5fb2e17ffa5ed9958
vkiet <veerakiet4@gmail.com>
2014-07-19 15:16:14 +0700

    edit make-coverage

--- make-coverage.sh
+++ make-coverage.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+rm -r coverage
+mkdir coverage
+
 # make unit
 mocha --reporter XUnit > ./coverage/xunit.xml
 
 # make coverage
-istanbul cover _mocha *.test.js
+istanbul cover _mocha
 istanbul report cobertura
