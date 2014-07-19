6eb5076a40da868fcbe791009ec27cd77a5f219c
vkiet <veerakiet4@gmail.com>
2014-07-19 16:01:06 +0700

    commit grade a-

--- test/grade.unit.test.js
+++ test/grade.unit.test.js
@@ -3,14 +3,16 @@ var grade = require('../app/grader').grade;
 
 describe('GradeUnitTest', function () {
 	describe('grade', function () {
-		it('should return A+', function () {
+		it('should return A+ when score is greater than 94', function () {
 			grade(96).should.equal('A+');
 		});
-	});
 
-	describe('grade', function () {
-		it('should return A', function () {
+		it('should return A when score is greater than 85', function () {
 			grade(93).should.equal('A');
+		});
+
+		it('should return A- when score is greater than 80', function () {
+			grade(82).should.equal('A-');
 		});
 	});
 });
