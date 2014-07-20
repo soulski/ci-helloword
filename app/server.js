b7d212f0925fdd20524a24f1c1b70256f2823164
SouLsKi <soulski@gmail.com>
2014-07-20 15:17:38 +0700

    merge code

--- app/server.js
+++ app/server.js
@@ -3,16 +3,21 @@ var express = require('express'),
 		grader = require('./grader');
 
 var app = express();
+
 app.engine('html', engines.hogan);
 app.set('view engine', 'html');
 app.set('views', 'views');
+
+app.use('/public', express.static(__dirname + '/../public'));
+
+console.log(__dirname + '/../public');
 
 app.get('/', function (req, res) {
 	res.redirect('home');
 });
 
 app.get('/home', function (req, res) {
-	res.render('index');
+	res.render('home');
 });
 
 app.get('/grade', function (req, res) {		
