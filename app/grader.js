be4fc0d2a08a25686c4309f9abfa48a4dfa8e91f
SouLsKi <soulski@gmail.com>
2014-07-20 14:46:49 +0700

    add express view render

--- app/grader.js
+++ app/grader.js
@@ -1,29 +1,32 @@
 var grade = function (score) {
-	if (score >= 95 && score <= 100) {
+	if (score > 100) {
+		return '';
+	}
+	else if (score >= 95) {
 		return 'A+';
 	}
-	else if (score >= 85 && score <= 94) {
+	else if (score >= 85) {
 		return 'A';
 	}
-	else if (score >= 80 && score <= 84) {
+	else if (score >= 80) {
 		return 'A-';
 	}
-	else if (score >= 70 && score <= 79) {
+	else if (score >= 70) {
 		return 'B';
 	}
-	else if (score >= 65 && score <= 69) {
+	else if (score >= 65) {
 		return 'C';
 	}
-	else if (score >= 60 && score <= 64) {
+	else if (score >= 60) {
 		return 'D+';
 	}
-	else if (score >= 55 && score <= 59) {
+	else if (score >= 55) {
 		return 'D';
 	}
-	else if (score >= 50 && score <= 54) {
+	else if (score >= 50) {
 		return 'D-';
 	}
-	else if (score >= 40 && score <= 49) {
+	else if (score >= 40) {
 		return 'F+';
 	}
 	else {
--- app/index.js
+++ app/index.js
@@ -1,15 +1,15 @@
 exports.sayHello = function () {
 	return "hello";
-}
+};
 
 exports.isHuman = function () {
 	return false;
-}
+};
 
 exports.isRobot = function () {
 	return false;
-}
+};
 
 exports.shout = function (name) {
 	return 'Shout : ' + name;
-}
+};
